f35338e416f6582cfc40eb68588a79192fbe51b4
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2024-01-26 16:00:30 -0600

    Fix import statement in hello.flow.ts

--- src/flow/hello.flow.ts
+++ src/flow/hello.flow.ts
@@ -1,4 +1,4 @@
-import { BotWhatsapp } from '@bot-whatsapp/bot';
+import BotWhatsapp from '@bot-whatsapp/bot';
 
 export default BotWhatsapp.addKeyword(['hola' , 'buenas'])
     .addAnswer('¡Hola! Bienvenido al Chatbot de Innova Web Marketing. Estoy aquí para ayudarte con tus preguntas y proporcionarte asistencia. ¿En qué puedo ayudarte hoy?')

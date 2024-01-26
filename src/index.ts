032827ff7a1b43d408ba9ceedc348e39cb198ca2
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2024-01-26 15:01:25 -0600

    Refactor code and add new features

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import MockAdaptar from '@bot-whatsapp/database/mock';
-import BotWhatsap from '@bot-whatsapp/bot';
-import ProviderWS from '@bot-whatsapp/provider/baileys';
+import MockAdaptar from '@bot-whatsapp/database/mock'
+import BotWhatsapp from '@bot-whatsapp/bot'
+import database from './database';
+import provider from './provider';
+import flow from './flow';
+import { initServer } from "./services/http";
+import { MockAdaptar } from '@bot-whatsapp/database/mock';
 
-/**
- * Instancia del bot
- */
-const flujoDeSaludar = BotWhatsapp.addkeyword(['hola', 'buenas'])
+const flujoDeSaludar = BotWhatsapp.addKeyword('hola' , 'buenas'])
 .addAnswer('¡Hola! Bienvenido al Chatbot de Innova Web Marketing. Estoy aquí para ayudarte con tus preguntas y proporcionarte asistencia. ¿En qué puedo ayudarte hoy?')
 
 /**
@@ -13,11 +14,20 @@ const flujoDeSaludar = BotWhatsapp.addkeyword(['hola', 'buenas'])
  */
 const main = async () => {
 
-    await BotWhatsapp.create({
-        database: new MockAdaptar(),
-        flow: BotWhatsapp.createFlow([]),
-        provider: BotWhatsapp.createProvider(ProviderWS)
+
+    const botFLow = BotWhatsapp.addKeyword('hola').addAnswer('Buenas!') as any
+
+    console.log(botFLow.toJson())
+    console.log({ botFLow })
+
+    const botInstance = await BotWhatsapp.createBot({
+        database,
+        provider,
+        flow
     })
+
+    initServer(botInstance)
 }
 
-main();
+
+main()

afa30b198c9ed91f4addf9b334a2d820e5c4fd7c
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2024-01-26 15:15:21 -0600

    "Update index.ts"

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-import MockAdaptar from '@bot-whatsapp/database/mock'
+import MockAdaptar from '@bot-whatsapp/database/mock';
 import BotWhatsapp from '@bot-whatsapp/bot'
-import database from './database';
-import provider from './provider';
-import flow from './flow';
-import { initServer } from "./services/http";
-import { MockAdaptar } from '@bot-whatsapp/database/mock';
-import { ProviderWS } from '@bot-whatsapp/provider/ws';
+import { ProviderWS } from '@bot-whatsapp/provider/baileys';
 
-const flujoDeSaludar = BotWhatsapp.addKeyword('hola' , 'buenas'])
+
+const flujoDeSaludar = BotWhatsapp.addKeyword(['hola' , 'buenas'])
 .addAnswer('¡Hola! Bienvenido al Chatbot de Innova Web Marketing. Estoy aquí para ayudarte con tus preguntas y proporcionarte asistencia. ¿En qué puedo ayudarte hoy?')
 
 /**
@@ -17,11 +13,10 @@ const main = async () => {
 
         await BotWhatsapp.createBot({
             database: new MockAdaptar(),
-            flow: BotWhatsapp.createFlow({flujoDeSaludar}),
-            provider: BotWhatsapp.createProvider({ProviderWS})
+            flow: BotWhatsapp.createFlow([flujoDeSaludar]),
+            provider: BotWhatsapp.createProvider(ProviderWS)
         })
 
 }
-
 
 main()

5de6969dba4b331e41eab77473217ec30f290c73
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2024-01-26 13:56:21 -0600

    Add keyword and answer for greeting message

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ import BotWhatsap from '@bot-whatsapp/bot';
 import ProviderWS from '@bot-whatsapp/provider/baileys';
 
 /**
+ * Instancia del bot
+ */
+const flujoDeSaludar = BotWhatsapp.addkeyword(['hola', 'buenas'])
+.addAnswer('¡Hola! Bienvenido al Chatbot de Innova Web Marketing. Estoy aquí para ayudarte con tus preguntas y proporcionarte asistencia. ¿En qué puedo ayudarte hoy?')
+
+/**
  * Funcion principal del bot
  */
-
 const main = async () => {
 
     await BotWhatsapp.create({

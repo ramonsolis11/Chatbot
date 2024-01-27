6498ad8cfb5e204ceb491a49098b463cf3d3bc85
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2024-01-26 20:44:07 -0600

    Update bot response message

--- src/flow/hello.flow.ts
+++ src/flow/hello.flow.ts
@@ -1,6 +1,9 @@
 import BotWhatsapp from '@bot-whatsapp/bot';
 
+/**
+ * implementación de la respuesta del bot a las palabras claves
+ */
 export default BotWhatsapp.addKeyword(['hola' , 'buenas'])
-    .addAnswer('¡Hola! Bienvenido al Chatbot de Innova Web Marketing. Estoy aquí para ayudarte con tus preguntas y proporcionarte asistencia. ¿En qué puedo ayudarte hoy?')
+    .addAnswer('¡Hola de nuevo! 😊 Es un placer verte por aquí. Cuéntame, ¿en qué puedo asistirte hoy? 👋')
 
     
